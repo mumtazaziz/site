a19e411b8696c483752c9029bae3498b440a972f
Mumtaz Aziz <mumtazaziz020305@gmail.com>
2023-04-11 04:20:04 +0700

    Use Nextra as middleware

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
+const nextra = require('nextra')
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx'
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
-    locales: ['en', 'id'],
+    locales: ['en', 'id', 'ja', 'ko'],
     defaultLocale: 'en'
   }
 }
 
-module.exports = nextConfig
+module.exports = withNextra(nextConfig)
